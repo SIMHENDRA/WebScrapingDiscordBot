f3760dc149e56e8fd695ea27afaf84f19f80dc4c
SIMHENDRA <ssn320@nyu.edu>
2020-08-25 20:40:01 -0400

    try except

--- bot.py
+++ bot.py
@@ -14,10 +14,15 @@ async def on_message(message):
         await message.channel.send("Defyn is a fat whore")
     if message.content.startswith('shame'):
         commands = message.content.split(" ")
-        if len(commands) == 3: 
-            await message.channel.send("```\nShame on " + commands[1] + "\n" + tsReq.req_stat_plane(commands[1],commands[2])+ "```")
-        elif len(commands) == 2:
-            await message.channel.send("```\n" + commands[1] + " is trash\n" + tsReq.req_worst(commands[1]) + "```" )
-        return
+        try:
+            if len(commands) == 3: 
+                await message.channel.send("```\nShame on " + commands[1] + "\n" + tsReq.req_stat_plane(commands[1],commands[2])+ "```")
+            elif len(commands) == 2:
+                await message.channel.send("```\n" + commands[1] + " is trash\n" + tsReq.req_worst(commands[1]) + "```" )
+            return
+        except:
+            await message.channel.send("```\nBad Input\n```")
+            return
+
 
 client.run()
